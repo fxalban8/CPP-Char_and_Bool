3c1ab2a5cd47e95e019f40be56f976425357d0f7
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 12:48:31 -0500

    Printing two char variables

--- Char_and_Bool.cpp
+++ Char_and_Bool.cpp
@@ -5,6 +5,15 @@ int main(){
 
 	bool state=true;	//declaring a bool variable
 	
+	char x=55;		/*declaring a char variable 
+				  which saves the character in 
+				  the ASCII table whose key value is 55
+				  */ 	
 
+	cout<<x<<endl;
+
+	char y='7';	//declaring a char variable which value is 7
+			//y cannot be used for arithmetic operations
+	cout<<y<<endl;
 	return 0;
 }
